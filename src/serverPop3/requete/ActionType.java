9159e0fb472b193d882906a1e43670b7b713f6be
Laura <premillieu.laura@gmail.com>
2015-03-04 11:42:54 +0100

    correction conflit

--- src/serverPop3/requete/ActionType.java
+++ src/serverPop3/requete/ActionType.java
@@ -1,22 +1,22 @@
 package serverPop3.requete;
 
 /**
- * Actions types réallisées par le serveur, classe mère des réactions au différentes requetes 
+ * Actions types réalisées par le serveur, classe mère des réactions au différentes requetes 
  * @author Corinne & Laura
  *
  */
 public abstract class ActionType {
-
+	protected static final String EXTENSION_MAIL = ".txt";
 	protected static final String MAIL_PATH = "./StockMail/";
 	
-	public String reponseOk(){
+	protected String reponseOk(String msg){
 		//TODO construire réponse ok
-		return "+OK ";
+		return "+OK "+msg;
 	}
 	
-	public String reponseKo(){
+	protected String reponseKo(String msg){
 		//TODO construire réponse ko
-		return "-KO ";
+		return "-KO "+msg;
 	}
 
 }
